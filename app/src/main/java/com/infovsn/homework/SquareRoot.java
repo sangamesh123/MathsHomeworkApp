b21f17d41889fd6cec27f0922b817ce1de403a16
Sangamesh Nanjayyanamath <sangameshnmath@gmail.com>
2025-10-13 21:36:32 +0530

    CubeRoot and SquareRoot Changes

--- app/src/main/java/com/infovsn/homework/SquareRoot.java
+++ app/src/main/java/com/infovsn/homework/SquareRoot.java
@@ -3,7 +3,8 @@ package com.infovsn.homework;
 import android.graphics.Color;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
 import android.text.method.ScrollingMovementMethod;
 import android.text.style.ForegroundColorSpan;
 import android.text.style.RelativeSizeSpan;
@@ -16,13 +17,21 @@ import android.widget.TextView;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class SquareRoot extends AppCompatActivity {
     private AdView mAdView;
     int ex=0;
     Button b1,b2,b3,b4,b5,b6,b7,b8,b9,b0,badd,bclr,back;
-    TextView et;
     ImageButton bsp, beq;
+    TextView et;
     TextView at;
+
+    // Buffer for keypad input
+    private final StringBuilder inputBuffer = new StringBuilder();
+    private static final int MAX_DIGITS = 8;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +40,6 @@ public class SquareRoot extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
-
 
         b1=(Button) findViewById(R.id.one);
         b2=(Button) findViewById(R.id.two);
@@ -49,152 +57,100 @@ public class SquareRoot extends AppCompatActivity {
         beq=(ImageButton) findViewById(R.id.equal);
         et=(TextView) findViewById(R.id.tabl);
         et.setMovementMethod(new ScrollingMovementMethod());
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                et.setText(et.getText()+"1");
-            }
-        });
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                et.setText(et.getText()+"2");
-            }
-        });
-        b3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                et.setText(et.getText()+"3");
-            }
-        });
-        b4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                et.setText(et.getText()+"4");
-            }
-        });
-        b5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                et.setText(et.getText()+"5");
-            }
-        });
-        b6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                et.setText(et.getText()+"6");
-            }
-        });
-        b7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                et.setText(et.getText()+"7");
-            }
-        });
-        b8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                et.setText(et.getText()+"8");
-            }
-        });
-        b9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                et.setText(et.getText()+"9");
-            }
-        });
-        b0.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                et.setText(et.getText()+"0");
-            }
-        });
 
-
-        badd.setOnClickListener(new View.OnClickListener() {
-
+        // Digit buttons
+        View.OnClickListener digitClick = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                val1=Integer.parseInt(et.getText()+"");
-//                add=true;
-                et.setText(et.getText()+".");
-            }
-        });
-
-        bclr.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-//                val1=Integer.parseInt(et.getText()+"");
-//                add=true;
-                et.setText(null);
-            }
-        });
-
-        bsp.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-//                val1=Integer.parseInt(et.getText()+"");
-//                add=true;
-                String smp = et.getText().toString();
-                if (smp.length() > 1) {
-                    char qq2=smp.charAt(smp.length()-1);
-
-                    smp = smp.substring(0, smp.length() - 1);
-                    et.setText(smp);
-                    char qq=smp.charAt(smp.length()-1);
-                    if(qq=='\n')
-                    {
-                        smp = smp.substring(0, smp.length() - 1);
-                        et.setText(smp);
-                    }
-                } else if (smp.length() <= 1) {
-                    et.setText(null);
+                int id = v.getId();
+                if (id == R.id.one) {
+                    appendDigit('1');
+                } else if (id == R.id.two) {
+                    appendDigit('2');
+                } else if (id == R.id.three) {
+                    appendDigit('3');
+                } else if (id == R.id.four) {
+                    appendDigit('4');
+                } else if (id == R.id.five) {
+                    appendDigit('5');
+                } else if (id == R.id.six) {
+                    appendDigit('6');
+                } else if (id == R.id.seven) {
+                    appendDigit('7');
+                } else if (id == R.id.eight) {
+                    appendDigit('8');
+                } else if (id == R.id.nine) {
+                    appendDigit('9');
+                } else if (id == R.id.zero) {
+                    appendDigit('0');
                 }
+            }
+        };
+        b1.setOnClickListener(digitClick);
+        b2.setOnClickListener(digitClick);
+        b3.setOnClickListener(digitClick);
+        b4.setOnClickListener(digitClick);
+        b5.setOnClickListener(digitClick);
+        b6.setOnClickListener(digitClick);
+        b7.setOnClickListener(digitClick);
+        b8.setOnClickListener(digitClick);
+        b9.setOnClickListener(digitClick);
+        b0.setOnClickListener(digitClick);
 
+        // Decimal point button
+        badd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                appendDot();
             }
         });
 
-        beq.setOnClickListener(new View.OnClickListener() {
-
+        // Clear button
+        bclr.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                inputBuffer.setLength(0);
+                updateEquationDisplay();
+            }
+        });
+
+        // Backspace button
+        bsp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (inputBuffer.length() > 0) {
+                    inputBuffer.deleteCharAt(inputBuffer.length() - 1);
+                }
+                updateEquationDisplay();
+            }
+        });
+
+        // Equals/Result button
+        beq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String cc = inputBuffer.toString();
                 setContentView(R.layout.added);
                 FontUtils.applyToActivity(SquareRoot.this);
                 at=(TextView) findViewById(R.id.txtScr);
                 at.setMovementMethod(new ScrollingMovementMethod());
-
-                //ADDS BY GOOGLE
                 mAdView=(AdView)findViewById(R.id.adView);
-//                mAdView.setAdListener(new ToastAdListener(SquareRoot.this));
                 AdRequest adRequest =new AdRequest.Builder().build();
-                mAdView.loadAd(adRequest);
-
-                String cc=et.getText().toString();
+                if (mAdView != null) {
+                    mAdView.loadAd(adRequest);
+                }
                 if(cc.length()>0)
-                squareroot();
+                    squareroot();
             }
         });
 
-
+        updateEquationDisplay();
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                // app icon in action bar clicked; goto parent activity.
                 this.finish();
                 return true;
             default:
@@ -202,33 +158,97 @@ public class SquareRoot extends AppCompatActivity {
         }
     }
 
-    private void squareroot()
-    {
-        boolean temp=false;
-        String sqr,sq;
-        double myNum = 0;
-        double num=999999999999999f;
-        myNum = Double.parseDouble(et.getText().toString());
-        if(myNum>num)
-        {
-            temp=true;
+    // Keypad screen: show only √N
+    private void updateEquationDisplay() {
+        if (et == null) return;
+        if (inputBuffer.length() == 0) {
+            et.setText(null);
+            return;
         }
-        sq="SqareRoot\n";
-        at.setText(et.getText());
-        at.append("\n\n");
-        SpannableString ss1=new SpannableString(sq);
-        ss1.setSpan(new RelativeSizeSpan(1.2f),0,ss1.length(),0);
-        ss1.setSpan(new ForegroundColorSpan(Colors.LCM_GREEN),0,ss1.length(),0);
-        at.append(ss1);
-       num=Math.sqrt(myNum);
+        String input = inputBuffer.toString();
+        SpannableStringBuilder builder = new SpannableStringBuilder();
+        builder.append("\u221A"); // Unicode square root symbol
+        builder.setSpan(new RelativeSizeSpan(1.4f), 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        int numStart = builder.length();
+        builder.append(input);
+        builder.setSpan(new RelativeSizeSpan(1.2f), numStart, builder.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        et.setText(builder);
+    }
 
-        sqr=String.format("%.2f",num)+"";
-        SpannableString ss2=new SpannableString(sqr);
-        ss2.setSpan(new RelativeSizeSpan(1.2f),0,ss2.length(),0);
-        ss2.setSpan(new ForegroundColorSpan(Color.RED),0,ss2.length(),0);
-        at.append(ss2);
+    private void appendDigit(char d) {
+        if (countDigits(inputBuffer) >= MAX_DIGITS) {
+            return;
+        }
+        inputBuffer.append(d);
+        updateEquationDisplay();
+    }
+
+    private void appendDot() {
+        if (indexOfDot(inputBuffer) >= 0) {
+            return;
+        }
+        if (inputBuffer.length() == 0) {
+            inputBuffer.append('0');
+        }
+        inputBuffer.append('.');
+        updateEquationDisplay();
+    }
+
+    private static int countDigits(CharSequence s) {
+        int c = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isDigit(s.charAt(i))) c++;
+        }
+        return c;
+    }
+
+    private static int indexOfDot(CharSequence s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '.') return i;
+        }
+        return -1;
+    }
+
+    private static Double tryParseDouble(String s) {
+        try {
+            if (s.equals("-") || s.equals(".")) return null;
+            return Double.parseDouble(s);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private static String formatNumber(double value) {
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(8, RoundingMode.HALF_UP).stripTrailingZeros();
+        String out = bd.toPlainString();
+        if (out.equals("-0")) out = "0";
+        return out;
+    }
+
+    // Result screen: show only √N = result (styled)
+    private void squareroot() {
+        boolean temp=false;
+        double num=99999999f;
+        Double parsed = tryParseDouble(inputBuffer.toString());
+        if (parsed == null) return;
+        double myNum = parsed;
+        if(myNum>num) temp=true;
+        SpannableStringBuilder eq = new SpannableStringBuilder();
+        eq.append("\u221A"); // Unicode square root symbol
+        eq.setSpan(new RelativeSizeSpan(1.4f), 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        int numStart = eq.length();
+        eq.append(formatNumber(myNum));
+        eq.setSpan(new RelativeSizeSpan(1.2f), numStart, eq.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        double rooted = Math.sqrt(myNum);
+        String res = formatNumber(rooted);
+        eq.append(" = ");
+        int resStart = eq.length();
+        eq.append(res);
+        eq.setSpan(new ForegroundColorSpan(Colors.LCM_GREEN), resStart, eq.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        eq.setSpan(new RelativeSizeSpan(1.2f), resStart, eq.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        at.setText(eq);
         if(temp)
-            at.setText("\"Maximum digits(15) exceeded\\n\"");
-        at.append("\n\n");
+            at.setText("Maximum digits(8) exceeded\n");
     }
 }
