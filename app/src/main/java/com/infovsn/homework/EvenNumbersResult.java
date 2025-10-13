74f665bb7fd49964ed852570ff5614fd0ff4ab1b
Sangamesh Nanjayyanamath <sangameshnmath@gmail.com>
2025-10-13 23:11:36 +0530

    Even Numbers Changes

---
+++ app/src/main/java/com/infovsn/homework/EvenNumbersResult.java
@@ -0,0 +1,58 @@
+package com.infovsn.homework;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+public class EvenNumbersResult extends AppCompatActivity {
+    private AdView mAdView;
+    private TextView mText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_even_numbers_result);
+        FontUtils.applyToActivity(this);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        // Ads by Google
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        if (mAdView != null) {
+            mAdView.loadAd(adRequest);
+        }
+
+        mText = findViewById(R.id.evenResult);
+
+        int startVal = getIntent().getIntExtra("start", 1);
+        int endVal = getIntent().getIntExtra("end", 500);
+        if (startVal > endVal) {
+            int tmp = startVal; startVal = endVal; endVal = tmp;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("Even numbers from ").append(startVal).append(" to ").append(endVal).append(":\n\n");
+        int first = (startVal % 2 == 0) ? startVal : (startVal + 1);
+        for (int i = first; i <= endVal; i += 2) {
+            sb.append(i).append("  ");
+        }
+        mText.setText(sb.toString());
+        mText.append("\n\n");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
+
